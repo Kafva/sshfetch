a2eff70e89d607ec01adb693715a8611eea952b0
Kafva <jonas@kafva.one>
2022-12-24 23:02:36 +0100

    -

--- Gemfile
+++ Gemfile
@@ -4,4 +4,4 @@ gem 'bcrypt_pbkdf', '~> 1.1'
 gem 'ed25519', '~> 1.3'
 gem 'net-ssh', '~> 7.0'
 
-gem "rubocop", "~> 1.41", :group => :development
+gem 'rubocop', '~> 1.41', group: :development
--- main.rb
+++ main.rb
@@ -1,6 +1,11 @@
+#!/usr/bin/env ruby
 require 'net/ssh'
 
-def do_ls(session)
+# 1. Parse ssh_config
+# 2. Create one thread per host
+# 3. Print.
+
+def get_sysinfo(session)
     session.open_channel do |channel|
         channel.on_data do |_ch, data|
             puts "[got data] -> #{data}"
@@ -9,7 +14,16 @@ def do_ls(session)
     end
 end
 
-Net::SSH.start('vel') do |session|
-    do_ls session
-    session.loop
+def open_ssh_connection(host)
+    Net::SSH.start(host) do |session|
+        get_sysinfo session
+        session.loop
+    end
 end
+
+def main
+    open_ssh_connection('vel')
+end
+
+#==============================================================================#
+main
