2ac736caaa1e796851e7741950dc013dd4f19645
Kafva <jonas@kafva.one>
2022-12-24 12:42:42 +0100

    linting fix

--- Gemfile
+++ Gemfile
@@ -1,8 +1,7 @@
 source 'https://rubygems.org'
 
+gem 'bcrypt_pbkdf', '~> 1.1'
+gem 'ed25519', '~> 1.3'
+gem 'net-ssh', '~> 7.0'
 
-
-gem "net-ssh", "~> 7.0"
-
-gem "ed25519", "~> 1.3"
-gem "bcrypt_pbkdf", "~> 1.1"
+gem "rubocop", "~> 1.41", :group => :development
--- main.rb
+++ main.rb
@@ -1,15 +1,15 @@
 require 'net/ssh'
 
-def do_ls( session )
-  session.open_channel do |channel|
-    channel.on_data do |ch, data|
-      puts "[got data] -> #{data}"
+def do_ls(session)
+    session.open_channel do |channel|
+        channel.on_data do |_ch, data|
+            puts "[got data] -> #{data}"
+        end
+        channel.exec 'uname'
     end
-    channel.exec "uname"
-  end
 end
 
-Net::SSH.start( 'vel' ) do |session|
-  do_ls session
-  session.loop
+Net::SSH.start('vel') do |session|
+    do_ls session
+    session.loop
 end
