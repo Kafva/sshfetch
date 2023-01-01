c22b6383a462f80e91039a46a80b4e4e2a4ca3a8
Kafva <jonas@kafva.one>
2023-01-01 22:13:53 +0100

    -

--- main.rb
+++ main.rb
@@ -15,53 +15,56 @@ LINUX_LOGOS = {
 # Each command should return one line of text (expect '<cmd>: ...' for errors)
 UNAME_CMD = 'uname -rms'
 
-MACOS_HW_CMD = 'system_profiler SPHardwareDataType -json -detailLevel mini|tr -d "\n" && echo'
+# !! No trailing newline !!
+MACOS_HW_CMD = 'system_profiler SPHardwareDataType -json -detailLevel mini | tr -d "\n"'
 
 LINUX_OS_CMD = 'grep "^ID=" /etc/os-release' 
 
 # Add newline on success
 LINUX_DEVTREE_CMD = 'cat /sys/firmware/devicetree/base/model && echo'
-LINUX_HW_CMD = 'cat /sys/devices/virtual/dmi/id/board_{name,version} 2>&1 | tr "\n" " " && echo'
+LINUX_HW_CMD = 'cat /sys/devices/virtual/dmi/id/board_{name,version} 2>&1 | tr "\n" " "'
 
 def info *args
     print "\e[34m>>>\e[0m "
     puts args
 end
 
-def err *args
-    eprint "\e[31m>>>\e[0m "
-    warn args
+def parse_os_release(out)
+  unless out.start_with?('grep: /etc/os-release: ')
+      name = out.split('=')[1]
+      LINUX_LOGOS[name]
+  end
 end
 
 def parse_macos_info(out)
   unless out.include?(': command not found: system_profiler')
-      return JSON.parse(out)['SPHardwareDataType'][0]['machine_model']
+      # Exclude output of next command: 'grep: /etc/os-release ...'
+      json_end = out.split('').rindex('}')
+      JSON.parse(out[0..json_end])['SPHardwareDataType'][0]['machine_model']
   end
-  return ''
 end
 
 def open_ssh_connection host
     Net::SSH.start(host) do |ssh|
         out = ssh.exec! [UNAME_CMD, MACOS_HW_CMD, LINUX_OS_CMD,
                          LINUX_DEVTREE_CMD, LINUX_HW_CMD, ].join(';')
-
         outlist = out.split("\n")
 
-        uname                 = outlist[0]
-        macos_info            = parse_macos_info(outlist[1])
-        linux_os_release      = outlist[2]
-        linux_info            = outlist[3]
-        linux_dev_tree        = outlist[4]
+        # warn outlist
+        # warn '=========='
 
-        puts macos_info
+        uname   = outlist[0]
+        hw_info = parse_macos_info(outlist[1])
 
-        #logo = LINUX_LOGOS[linux_os_release]
+        if hw_info.nil?
+            logo = parse_os_release(outlist[2])
+            hw_info  = !outlist[3].start_with?('cat: /sys') ? 
+                       outlist[3] : outlist[4]
+        else
+            logo = "\e[97m \e[0m"
+        end
 
-        #if logo.nil? and not macos_info.nil?
-        #    logo = "\e[97m \e[0m "
-        #end
-
-        #puts "#{logo} #{macos_info} #{uname}"
+        puts "#{logo} #{hw_info} #{uname}"
     end
 end
 
